118ee8e5de5946cd11c4680f6964795cfd92610b
Jonothen099 <96099265+Jonothen099@users.noreply.github.com>
2022-10-23 14:59:42 +1100

    Initial Commit

---
+++ CoinFolios/CoinFoliosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFoliosApp.swift
+//  CoinFolios
+//
+//  Created by Jono Jono on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoinFoliosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
